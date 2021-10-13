27c84fc05dc76c1c2a2bbde9180ee9bbc04b233a
Giovanni Castinelli <gcastinelli@infosis-arg.com>
2021-10-12 22:51:21 -0300

    stuff

--- data.js
+++ data.js
@@ -5,14 +5,14 @@ const apiUrl = "https://backend-farm.plantvsundead.com/get-plants-filter-v2";
 
 const getPlants = async function (token, offset = 0) {
   var elements = [];
-  var checked = document.getElementById("elements").getElementsByTagName("input");
+  var checked = document.getElementById('elements').getElementsByTagName('input');
 
   for (const i in checked) {
-      const element = checked[i];
-      
-      if (element.checked) {
-        elements.push(element.value)
-      }
+    const element = checked[i];
+
+    if (element.checked) {
+      elements.push(element.value);
+    }
   }
 
   let actualUrl = apiUrl + `?elements=${elements.join(',')}&offset=${offset}&limit=${limitPerPage}`;
@@ -23,58 +23,74 @@ const getPlants = async function (token, offset = 0) {
     credentials: 'same-origin', // include, *same-origin, omit
     headers: {
       'Content-Type': 'application/json',
-      'Authorization': 'Bearer ' + token
-    }}).then((resp) => {
-    return resp.json();
-  }).catch((error) => {
-    div.innerHTML = "";
-    console.error('Error:', error);
-  });
+      Authorization: 'Bearer ' + token,
+    },
+  })
+    .then((resp) => {
+      return resp.json();
+    })
+    .catch((error) => {
+      div.innerHTML = '';
+      console.error('Error:', error);
+    });
 
   return apiResults;
 };
 
 const getEntireUserList = async function (offset = 0) {
-  const results = await getPlants(document.getElementById("token").value, offset);
+  const results = await getPlants(document.getElementById('token').value, offset);
   if (results.data.length) {
-    return results.data.concat(await getEntireUserList(offset + 5000));
+    return results.data.concat(await getEntireUserList(offset + limitPerPage));
   } else {
     return results.data;
   }
 };
 
 async function findPlant() {
-  div = document.getElementById("data");
-  div.innerHTML = "Buscando...";
+  div = document.getElementById('data');
+  document.getElementById('searching').innerHTML = 'Buscando...';
   const entireList = await getEntireUserList();
   draw(filterResults(entireList));
 }
 
 function filterResults(data) {
-  const pvu = document.getElementById("pvu").value;
-  const le = document.getElementById("le").value;
+  const pvu = document.getElementById('pvu').value;
+  const le = document.getElementById('le').value;
 
-  const result = data.filter((element) => {
-    if (element["config"] ? element["config"].farm.le / element["config"].farm.hours >= le && element["startingPrice"] <= pvu : false) {
-      return true;
-    }
-    return false;
-  });
-  result.sort((a, b) =>
-    a["config"].farm.le / a["config"].farm.hours < b["config"].farm.le / b["config"].farm.hours ? 1 : b["config"].farm.le / b["config"].farm.hours < a["config"].farm.le / a["config"].farm.hours ? -1 : 0
-  );
+  if (data) {
+    const result = data.filter((element) => {
+      if (
+        element['config']
+          ? element['config'].farm.le / element['config'].farm.hours >= le && element['startingPrice'] <= pvu
+          : false
+      ) {
+        return true;
+      }
+      return false;
+    });
+    result.sort((a, b) =>
+      a['config'].farm.le / a['config'].farm.hours < b['config'].farm.le / b['config'].farm.hours
+        ? 1
+        : b['config'].farm.le / b['config'].farm.hours < a['config'].farm.le / a['config'].farm.hours
+        ? -1
+        : 0,
+    );
 
-  return result;
+    return result;
+  }
+
+  return undefined;
 }
 
 function draw(data) {
-  div.innerHTML = "";
+  div.innerHTML = '';
   if (data && data.length > 0) {
-  data.forEach(function (element) {
-    div.innerHTML += addPlant(element);
-  });  
+    data.forEach(function (element) {
+      div.innerHTML += addPlant(element);
+    });
+    document.getElementById('searching').innerHTML = '';
   } else {
-    div.innerHTML += "No se encontraron resultados";
+    document.getElementById('searching').innerHTML = 'No se encontraron resultados';
   }
 }
 
